ee373f5536c08c63a1da76a58cacfaccd98aaa35
ryota-1231 <rrrrr.1231@gmail.com>
2019-09-05 17:43:30 +0900

    correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(20)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user)
     respond_to do |format|
       format.html
       format.json

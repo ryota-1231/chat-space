9da16976a62bedd406d48f6728fbd7e1f5902ad9
ryota-1231 <rrrrr.1231@gmail.com>
2019-09-06 12:49:02 +0900

    setting

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,12 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-    @message.save
-   
+     @message.save
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' } 
         format.json
+      
+
       end
   end
 

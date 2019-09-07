50eb0c2f0467a73b0f882e225539cfe311dbff90
ryota-1231 <rrrrr.1231@gmail.com>
2019-09-07 12:26:19 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,13 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_provider = 'fog/aws'
+  
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1' #例 'ap-northeast-1'
+    config.fog_provider = 'fog/aws'
   }
 
   config.fog_directory  = 'rrrrr1231'

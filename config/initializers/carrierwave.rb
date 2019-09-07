c0c63c61e8ea0a259d0a7ce800c8496d8bfcd4ee
ryota-1231 <rrrrr.1231@gmail.com>
2019-09-07 12:30:10 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,9 +10,9 @@ CarrierWave.configure do |config|
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1',
-    config.fog_provider = 'fog/aws',
+    
   }
-
+  config.fog_provider = 'fog/aws'
   config.fog_directory  = 'rrrrr1231'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/rrrrr1231'
 end
